dee138b2c7278f28928279909e892aced6aa1f9d
Виктор Губанов <adramikon@mail.ru>
2023-06-22 12:15:57 +0300

    refactoring

--- TTSVFileStream.cfg.js
+++ TTSVFileStream.cfg.js
@@ -1,5 +1,5 @@
 //ver alt0_0WAlt0test0W
 
-exports.fileReadBuffer={
-	size: 16*1024 // 16kb
+exports.fileReadBuffer = {
+    size: 16 * 1024 // 16kb
 };
--- TTSVFileStream.lib.js
+++ TTSVFileStream.lib.js
@@ -1,121 +1,90 @@
 //ver alt0_3WAlt0test0W
 
-var readline=require('readline');
+const readline = require('readline');
 
-var fs=require('fs');
+const fs = require('fs');
 
-var myUtil=require('./myUtil.lib.js');
+const myUtil = require('./myUtil.lib.js');
 
-var SETTINGS=require('./TTSVFileStream.cfg.js');
+const SETTINGS = require('./TTSVFileStream.cfg.js');
 
-module.exports=function(
-	params/*={
+module.exports = function (
+    params/*={
 		fileNames: [/ * ... * /]
 	}*/
-)
-{
-	var vm=this;
-	
-	vm.fileNames=params.fileNames;
-	
-	vm.currFileNameIdx=0;
-	
-	vm.accum='';
-	
-	vm.fd=null;
-	
-	vm.buffer=Buffer.allocUnsafe(SETTINGS.fileReadBuffer.size);
-	
-	vm.cleanup=function()/*ver alt0_0WAlt0test0W .*/
-	{
-		if(myUtil.isDefined(vm.fd))
-		{
-			fs.closeSync(vm.fd);
-			vm.fd=null;
-		}
-		vm.buffer=null;
-	};
-	
-	vm.parseLine=function(line)/*ver alt0_0WAlt0test0W .*/
-	{
-		if(myUtil.isDefined(line) && !!line)
-		{
-			return line.split('\t');
-		}
-		return null;
-	};
-	
-	vm.reinitFd=function()/*ver alt0_0WAlt0test0W .*/
-	{
-		if(myUtil.isDefined(vm.fd))
-		{
-			fs.closeSync(vm.fd);
-			vm.fd=null;
-		}
-		if(vm.currFileNameIdx<vm.fileNames.length)
-		{
-			vm.fd=fs.openSync(vm.fileNames[vm.currFileNameIdx++],'r');
-			return true;
-		}
-		return false;
-	};
-	
-	vm.getChunk=function()/*ver alt0_1WAlt0test0W .*/
-	{
-		/*< ...(1)>*/
-		var bytesRead=(
-			myUtil.isDefined(vm.fd)?
-				fs.readSync(
-					vm.fd, 
-					vm.buffer,
-					0,
-					vm.buffer.length,
-					null
-				)
-			:
-				0
-		);
-		/*</...(1)>*/
-		if(bytesRead==0)
-		{
-			return vm.reinitFd()?vm.getChunk():null;
-		}
-		return vm.buffer.slice(0, bytesRead).toString();
-	};
-	
-	vm.getLine=function()/*ver alt0_1WAlt0test0W .*/
-	{
-		var newLineIdx=-1;
-		while((newLineIdx=vm.accum.indexOf('\n'))===-1)
-		{
-			var chunk=vm.getChunk();
-			if(!chunk)
-			{
-				return vm.parseLine(vm.accum);
-			}
-			vm.accum+=chunk;
-		}
-		var line=vm.accum.slice(0,newLineIdx);
-		vm.accum=vm.accum.slice(newLineIdx+1);
-		return vm.parseLine(line);
-	};
-	
-	vm.reinitFd();
+) {
+    let vm = this;
+
+    let _fileNames = params.fileNames;
+
+    let _currFileNameIdx = 0;
+
+    let _accum = '';
+
+    let _fd = null;
+
+    let _buffer = Buffer.allocUnsafe(SETTINGS.fileReadBuffer.size);
+
+    vm.getLine = function ()/*ver alt0_1WAlt0test0W .*/ {
+        let newLineIdx = -1;
+        while ((newLineIdx = _accum.indexOf('\n')) === -1) {
+            let chunk = getChunk();
+            if (!chunk) {
+                return parseLine(_accum);
+            }
+            _accum += chunk;
+        }
+        let line = _accum.slice(0, newLineIdx);
+        _accum = _accum.slice(newLineIdx + 1);
+        return parseLine(line);
+    };
+
+    function getChunk() {
+        /*< ...(1)>*/
+        var bytesRead = (
+            myUtil.isDefined(_fd)
+                ? fs.readSync(
+                    _fd,
+                    _buffer,
+                    0,
+                    _buffer.length,
+                    null
+                )
+                : 0
+        );
+        /*</...(1)>*/
+        if (bytesRead === 0) {
+            return reinitFd() ? getChunk() : null;
+        }
+        return _buffer.slice(0, bytesRead).toString();
+    }
+
+    function reinitFd() {
+        if (myUtil.isDefined(_fd)) {
+            fs.closeSync(_fd);
+            _fd = null;
+        }
+        if (_currFileNameIdx < _fileNames.length) {
+            _fd = fs.openSync(_fileNames[_currFileNameIdx++], 'r');
+            return true;
+        }
+        return false;
+    }
+
+    function parseLine(line) {
+        if (myUtil.isDefined(line) && !!line) {
+            return line.split('\t');
+        }
+        return null;
+    }
+
+    vm.cleanup = function ()/*ver alt0_0WAlt0test0W .*/ {
+        if (myUtil.isDefined(_fd)) {
+            fs.closeSync(_fd);
+            _fd = null;
+        }
+        _buffer = null;
+    };
+
+    reinitFd();
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
